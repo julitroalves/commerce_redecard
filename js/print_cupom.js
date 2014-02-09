5ea83a2aa23bf43ff16cb13c65f4fd46a62c2fd0
Julio Alves <julitroalves@gmail.com>
2014-02-09 14:04:11 -0300

    Added javascript coding standards to js file;

--- js/print_cupom.js
+++ js/print_cupom.js
@@ -1,5 +1,16 @@
-function print_cupom() {
-  var print_path = Drupal.settings['peticoes_redecard']['print_path'];
+/**
+ * @file
+ * This file provides a listener event to click button of payment page.
+ */
 
-  return window.open(print_path, '_blank', 'width=280,height=440,left=0,top=100,screenX=0,screenY=100');
-}
+(function ($) {
+  Drupal.behaviors.commerceRedecard = {
+    attach: function (context, settings) {
+      $('.super-button', context).click(function () {
+        var print_path = Drupal.settings['commerce_redecard']['print_path'];
+
+        return window.open(print_path, '_blank', 'width=280,height=440,left=0,top=100,screenX=0,screenY=100');
+      });
+    }
+  };
+})(jQuery);
